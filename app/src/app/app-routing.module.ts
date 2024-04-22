d4ddf805a008875bcb8dba58c199e7dd397a7b28
mark oliver meguizo <mark.meguizo@gmail.com>
2024-04-22 15:20:06 +0800

    test auth0 login

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -50,8 +50,10 @@ export const routes: Routes = [
             },
         ],
     },
-    { path: '', redirectTo: 'pages', pathMatch: 'full' },
-    { path: '**', redirectTo: 'pages' },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    // { path: '', redirectTo: 'pages', pathMatch: 'full' },
+    // { path: '**', redirectTo: 'pages' },
 ];
 
 const config: ExtraOptions = {
